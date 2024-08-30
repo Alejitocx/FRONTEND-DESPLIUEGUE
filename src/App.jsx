a446017e40d2483c427ba5af71986b2cb8db8920
Alejitocx <itsalejo12@gmail.com>
2024-08-29 21:23:15 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import './App.css';
 import LoginPage from './components/login.jsx';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -5,17 +6,6 @@ import ProductosList from './components/productos.jsx';
 import SignUpForm from './components/SingUp.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import { useState } from 'react';
-
-// Componente para mostrar un mensaje de error cuando la ruta no existe
-function NotFound() {
-  return (
-    <div className="not-found">
-      <h2>404: Página No Encontrada</h2>
-      <p>Lo sentimos, la página que buscas no existe.</p>
-      <button onClick={() => window.history.back()}>Volver</button>
-    </div>
-  );
-}
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -35,18 +25,17 @@ function App() {
         <Route path="/SignUpForm" element={<SignUpForm />} />
         <Route
           path="/ProductosList"
-          element={
-            <ProtectedRoute
-              isAuthenticated={isAuthenticated}
-              element={<ProductosList />}
-            />
-          }
-        />
-        {/* Ruta para manejar URLs no existentes */}
-        <Route path="*" element={<NotFound />} />
+           element={
+    <ProtectedRoute
+      isAuthenticated={isAuthenticated}
+      element={<ProductosList />}
+    />
+   }
+    />  
       </Routes>
     </Router>
   );
 }
 
 export default App;
+

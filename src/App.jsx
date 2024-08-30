4c05d6db675889c26e66ed4b913ebb7dbe17f008
Alejitocx <itsalejo12@gmail.com>
2024-08-29 21:51:38 -0500

    finale

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css';
 import LoginPage from './components/login.jsx';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -17,22 +16,30 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Redirige a la página de inicio de sesión por defecto */}
         <Route path="/" element={<Navigate to="/LoginPage" />} />
+        
+        {/* Ruta para la página de inicio de sesión */}
         <Route 
           path="/LoginPage" 
           element={<LoginPage onLogin={handleLogin} />} 
         />
-        <Route path="/SignUpForm" element={<SignUpForm />} />
-        <Route
-  path="/ProductosList"
-  element={
-    <ProtectedRoute
-      isAuthenticated={isAuthenticated}
-      element={<ProductosList />}
-    />
-  }
-/>
 
+        {/* Ruta para el formulario de registro */}
+        <Route path="/SignUpForm" element={<SignUpForm />} />
+        
+        {/* Ruta protegida para la lista de productos */}
+        <Route
+          path="/ProductosList"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <ProductosList />
+            </ProtectedRoute>
+          }
+        />
+        
+        {/* Redirigir rutas no encontradas a la página de inicio de sesión */}
+        <Route path="*" element={<Navigate to="/LoginPage" />} />
       </Routes>
     </Router>
   );
